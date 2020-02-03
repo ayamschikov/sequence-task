6915f567a575cd9367113c1a7e98ab3c58a3c43a
Aleksandr Yamschikov <yamshchikov.as73@gmail.com>
2020-02-03 04:49:19 +0400

    Separate tests

--- test/sequence_test.rb
+++ test/sequence_test.rb
@@ -8,10 +8,13 @@ class SequenceTest < Minitest::Test
     @sequence = Sequence.new('1')
   end
 
-  def test_first
+  def test_get_next_value
     assert_equal '11', @sequence.next.value
     assert_equal '21', @sequence.next.value
-    assert_equal '1211', @sequence.next.value
+  end
+
+  def test_use_several_next_method
+    assert_equal '1211', @sequence.next.next.next.value
     assert_equal '13112221', @sequence.next.next.next.value
   end
 end
